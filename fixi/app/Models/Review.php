62aae36ed42bb106f8e11cdc181d8a4eaedaf372
Gabriel Maciel <wowgdm@gmail.com>
2025-09-05 16:47:54 -0300

    Review Model

--- fixi/app/Models/Review.php
+++ fixi/app/Models/Review.php
@@ -4,9 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    /** @use HasFactory<\Database\Factories\ReviewFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'client_id',
+        'provider_id',
+        'rating',
+        'comment',
+        'is_visible',
+    ];
+
+    protected $casts = [
+        'rating' => 'integer',
+        'is_visible' => 'boolean',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function provider(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'provider_id');
+    }
 }
