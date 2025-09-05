06839155898cef126f31a2915cd6442b5442980e
Gabriel Maciel <wowgdm@gmail.com>
2025-09-05 16:44:52 -0300

    Service Model

--- fixi/app/Models/Service.php
+++ fixi/app/Models/Service.php
@@ -4,9 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Service extends Model
 {
-    /** @use HasFactory<\Database\Factories\ServiceFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'unity_of_measure',
+        'icon',
+        'is_active',
+        'category_id',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(ServiceCategory::class, 'category_id');
+    }
+
+    public function providers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'provider_services', 'service_id', 'provider_user_id')
+            ->withPivot(['base_price', 'description'])
+            ->withTimestamps();
+    }
 }
