30146b48e525636a8edaa6998dcef7a791346898
Gabriel Maciel <wowgdm@gmail.com>
2025-09-05 16:46:06 -0300

    Service Model

--- fixi/app/Models/ProviderService.php
+++ fixi/app/Models/ProviderService.php
@@ -2,11 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class ProviderService extends Model
+class ProviderService extends Pivot
 {
-    /** @use HasFactory<\Database\Factories\ProviderServiceFactory> */
-    use HasFactory;
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    protected $table = 'provider_services';
+
+    protected $fillable = [
+        'service_id',
+        'provider_id',
+        'base_price',
+        'description',
+    ];
+
+    protected $casts = [
+        'base_price' => 'decimal:2',
+    ];
 }
